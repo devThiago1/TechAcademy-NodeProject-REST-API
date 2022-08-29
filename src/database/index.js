4d44f06d6ffdcc23b5482552a565369e49dde4b8
devThiago1 <thiago.calmon4@gmail.com>
2022-08-28 23:16:13 -0300

    50%

---
+++ src/database/index.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+
+const connectToMySql = async () => {
+
+    const config = {
+        host: "localhost",
+        user: "root",
+        password: "senha_root",
+        database: "tech_academy"
+    }
+
+
+
+}
