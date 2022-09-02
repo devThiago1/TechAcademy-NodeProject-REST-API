be10f51bfc21eee87c108b9d29c0c8803053709a
devThiago1 <thiago.calmon4@gmail.com>
2022-09-01 22:45:22 -0300

    conexão com MySql informando erro

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,21 @@
 const mysql = require('mysql2/promise');
 
 const connectToMySql = async () => {
+    if(global.connection && global.connection.state !== 'disconnected')
+        return global.connection;
 
     const config = {
-        host: "localhost",
+        host: "127.0.0.1",
         user: "root",
-        password: "senha_root",
-        database: "tech_academy"
+        password: "Leao1899*",
+        database: "tech-academy"
     }
-
-
-
+    
+    const connection = await mysql.createConnection(config);
+    console.log("Conexão realizada com sucesso");
+    global.connection = connection;
 }
+
+connectToMySql();
+
+module.exports = { connectToMySql };
--- src/database/repositories/studentRepository.js
+++ src/database/repositories/studentRepository.js
@@ -1,28 +1,10 @@
+const db = require('../index');
 class StudentRepository {
     async findAll() {
-      const alunos = [
-        {
-          code: 12345,
-          name: 'José da Silva',
-          dob: '2000-07-09'
-        },
-        {
-          code: 12346,
-          name: 'Joana Souza',
-          dob: '1998-01-11'
-        },
-        {
-          code: 12347,
-          name: 'Lucas Gomes',
-          dob: '1992-07-17'
-        },
-        {
-          code: 12348,
-          name: 'Maria Eduarda',
-          dob: '2001-02-09'
-        },
-      ];
-      return alunos;
+      const conn = await db.connectToMySql();
+      const query = "SELECT * FROM student";
+      const students = await conn.query(query);
+      return students;
     }
   }
   
